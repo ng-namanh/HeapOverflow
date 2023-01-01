5d40a1ea95bd64a637f3c2729575be51316f2b42
Hiephanu <doanvanhiep230903@gmail.com>
2023-01-02 03:36:16 +0700

    update

--- components/Ready.vue
+++ components/Ready.vue
@@ -1,35 +1,35 @@
 <template>
   <section class="mt-[80px]">
-    <div class="flex relative justify-between">
-      <img class="w-[100px] h-[100px] mt-[1rem]" src="../Public/Image/image10.png" alt="" />
-      <img class="w-[150px] h-[150px] " src="../Public/Image/image15.png" alt="" />
-      <img class="w-[120px] h-[120px] " src="../Public/Image/image13.png" alt="" />
+    <div class="flex relative justify-between sm:justify-around">
+      <img class="w-[100px] h-[100px] mt-[1rem] sm:w-8 sm:h-8" src="../Public/Image/image10.png" alt="" />
+      <img class="w-[150px] h-[150px] sm:w-8 sm:h-8" src="../Public/Image/image15.png" alt="" />
+      <img class="w-[120px] h-[120px] sm:w-8 sm:h-8 sm:mt-[0.75rem]" src="../Public/Image/image13.png" alt="" />
     </div>
     <div class="flex relative">
-      <img class="absolute top-[7rem] left-[5%]" src="~~/Public/Image/image14.png" alt="">
-      <img class="absolute top-[7rem] left-[85%]" src="~~/Public/Image/image17.png" alt="">
+      <img class="absolute top-[7rem] left-[5%] sm:w-[15px] sm:h-[15px] sm:top-12" src="~~/Public/Image/image14.png" alt="">
+      <img class="absolute top-[7rem] left-[85%] sm:w-[20px] sm:h-[20px] sm:top-12" src="~~/Public/Image/image17.png" alt="">
     </div>
-    <div class="mt-[58px] flex flex-col items-center justify-center">
-      <h1 class="text-white text-[54px] leading-[80px] font-bold text-center">
+    <div class="mt-[58px] flex flex-col items-center justify-center sm:mt-2">
+      <h1 class="text-white text-[54px] leading-[80px] font-bold text-center sm:text-[16px] sm:leading-[24px]">
         Ready to use <br />
         Heap Overflow
       </h1>
       <p
-        class="text-white text-[26px] leading-[48px] text-center font-medium mt-6"
+        class="text-white text-[26px] leading-[48px] text-center font-medium mt-6 sm:text-[10px] sm:w-[170px] sm:leading-[12px] sm:mt-2"
       >
         Join thousand users and teams in the community
       </p>
-      <div class=" mt-12">
+      <div class=" mt-12 sm:mt-6">
         <button
-          class="text-base py-3 sm:py-2 px-8 sm:px-6 rounded-xl font-medium bg-[#1E50FF] text-white transition-all border border-transparent hover:bg-white hover:border-black hover:text-black"
+          class="text-base py-3 px-8 rounded-xl font-medium bg-[#1E50FF] text-white transition-all border border-transparent hover:bg-white hover:border-black hover:text-black sm:w-6 sm:h-6 sm:text-xs"
         >
           Get start
         </button>
       </div>
     </div>
-    <div class="flex justify-between ">
-        <img class="w-[100px] h-[100px]" src="~~/Public/Image/image11.png" alt="">
-        <img class="w-[100px] h-[100px]" src="~~/Public/Image/image16.png" alt="">
+    <div class="flex justify-between sm:justify-around">
+        <img class="w-[100px] h-[100px] sm:w-5 sm:h-5" src="~~/Public/Image/image11.png" alt="">
+        <img class="w-[100px] h-[100px] sm:w-5 sm:h-5" src="~~/Public/Image/image16.png" alt="">
       </div>
   </section>
 </template>
--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,52 +1,54 @@
 <template>
     <section class="">
-        <div class="flex justify-around mt-desktop">
+        <div class="flex sm:grid justify-around mt-desktop">
             <div>
                 <div class="flex gap-2 items-center">
                     <img class=" w-9"  src="~~/assets/image/logo.svg" alt="">
-                    <h2 class="font-sans text-sm text-white font-bold">Heap Overflow</h2>
+                    <h2 class="font-sans text-sm text-white font-bold sm:text-sm">Heap Overflow</h2>
                 </div>
-                <p class="font-sans text-xs text-white font-normal leading-8 h-32 w-[250px] mt-[24px]">
+                <p class="font-sans text-xs text-white font-normal leading-8 h-32 w-64 mt-6 sm:text-xs">
                     Heap Overflow is the best Online Judge for Technical Student and Devloper to improve technical knowlege by solving programing problems and participanting contest 
                 </p>
             </div>
-            <div class="">
-                <h1 class="font-sans text-2xl font-bold text-white">
-                    Resources
-                </h1>
-                <div class="font-san text-xs font-light text-white mt-9">
-                        <NuxtLink to="/">Help center</NuxtLink>
+            <div class="flex sm:flex">
+                <div class="">
+                    <h1 class="font-sans text-2xl font-bold text-white">
+                        Resources
+                    </h1>
+                    <div class="font-san text-xs font-light text-white mt-9">
+                            <NuxtLink to="/">Help center</NuxtLink>
+                    </div>
+                    <div class="font-san text-xs font-light text-white mt-4">
+                            <NuxtLink to="/">Partner</NuxtLink>
+                    </div>
                 </div>
-                <div class="font-san text-xs font-light text-white mt-[16px]">
-                        <NuxtLink to="/">Partner</NuxtLink>
+                <div class="ml-24 sm:ml-16">
+                    <h1 class="font-sans text-2xl font-bold text-white">
+                        Organizer
+                    </h1>
+                    <div class="font-san text-xs font-light text-white mt-9">
+                            <NuxtLink to="/">Our team</NuxtLink>
+                    </div>
+                    <div class="font-san text-xs font-light text-white mt-4">
+                            <NuxtLink to="/">About us</NuxtLink>
+                    </div>
+                    <div class="font-san text-xs font-light text-white mt-4">
+                            <NuxtLink to="/">Contact us</NuxtLink>
+                    </div>
                 </div>
             </div>
-            <div class="">
-                <h1 class="font-sans text-2xl font-bold text-white">
-                    Organizer
-                </h1>
-                <div class="font-san text-xs font-light text-white mt-9">
-                        <NuxtLink to="/">Our team</NuxtLink>
-                </div>
-                <div class="font-san text-xs font-light text-white mt-[16px]">
-                        <NuxtLink to="/">About us</NuxtLink>
-                </div>
-                <div class="font-san text-xs font-light text-white mt-[16px]">
-                        <NuxtLink to="/">Contact us</NuxtLink>
-                </div>
-            </div>
-            <div class="">
+            <div class="ml-8 sm:ml-0 sm:mt-9">
                 <h1 class="font-sans text-2xl font-bold text-white">
                     Contact
                 </h1>
-                <p class=" text-xs font-light text-white w-[179px] h-[96px] mt-[24px] leading-8">
+                <p class=" text-xs font-light text-white w-44 h-24 mt-6 leading-8">
                     416C, Hanoi University
                     Km9, Nguyen Trai, Thanh Xuan, Hanoi
                 </p>
                 <div class="flex">
-                    <img class="w-[32px]" src="~~/Public/Image/Facebook.png" alt="">
-                    <img class="w-[32px] ml-[40px]" src="~~/Public/Image/instagram (1).png" alt="">
-                    <img class="w-[32px] ml-[40px]" src="~~/Public/Image/Vector (1).png" alt="">
+                    <img class="w-[32px]" src="~~/Public/Image/facebook.png" alt="">
+                    <img class="w-[32px] ml-[40px]" src="~~/Public/Image/insta.png" alt="">
+                    <img class="w-[32px] ml-[40px]" src="~~/Public/Image/linkedin.png" alt="">
                 </div>
             </div>
         </div>
